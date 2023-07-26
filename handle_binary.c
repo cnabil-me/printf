4c65a0e691f18b8871d0036b7bccf4e6f5c9931e
cnabilgit <46906074+cnabilhub@users.noreply.github.com>
2023-07-26 01:07:42 +0100

    starting betty docs

--- handle_binary.c
+++ handle_binary.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
  * handle_binary - print character and return the print count
+ * @format: the format
+ * @value: the value to print
+ * Return:counter of printed chars
  */
 
 int handle_binary(const char *str, char *value)
